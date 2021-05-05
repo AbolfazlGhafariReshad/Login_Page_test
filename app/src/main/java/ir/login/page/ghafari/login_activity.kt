17c5fa580ae5aa6cdd358fea01697af2ba2f8c65
Abolfazl Ghafari <ghr.abolfazl@yahoo.com>
2021-05-05 22:02:42 +0430

    Initial commit

---
+++ app/src/main/java/ir/login/page/ghafari/login_activity.kt
@@ -0,0 +1,11 @@
+package ir.login.page.ghafari
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class login_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_activity)
+    }
+}
